2102e39fdb728191b75338b871e894caa2e924cc
Theo Moura <theomouram@gmail.com>
2020-10-10 15:54:41 -0300

    Update mongo.initialization.ts

--- src/mongo/mongo.initialization.ts
+++ src/mongo/mongo.initialization.ts
@@ -2,6 +2,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 export function initMongo() {
   return MongooseModule.forRoot(
-    'mongodb+srv://admin:admin@cluster0.y7bzx.mongodb.net/shopping-cart?retryWrites=true&w=majority',
+    'mongodb+srv://admin:<passwd>@cluster0.y7bzx.mongodb.net/shopping-cart?retryWrites=true&w=majority',
   );
 }
